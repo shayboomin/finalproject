7a36c2777a7972afd43dc956c7a7a53e40603b7e
Kris <krisekenes@gmail.com>
2017-01-13 08:35:37 -0800

    merging with master

--- finalproject/config/routes.rb
+++ finalproject/config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
 
-  get '/', to: 'BoominTravel#index'
-=======
  get '/', to: 'boomintravel#index'
  get '/login', to: 'boomintravel#user'
  get '/hotel', to: 'boomintravel#hotel'
  get '/flight', to: 'boomintravel#flight'
->>>>>>> 273ad9bb426c61985b98640da80bac1a6105b97d
+
 end
