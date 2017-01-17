c635f3ffaf2a3a5a4a36f3be54aedfaad296d374
Kris <krisekenes@gmail.com>
2017-01-17 15:25:31 -0800

    fixed application controller conflict

--- finalproject/app/controllers/application_controller.rb
+++ finalproject/app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-<<<<<<< HEAD
-  def index
-    render 'login'
-  end
-
-=======
- 
->>>>>>> 273ad9bb426c61985b98640da80bac1a6105b97d
 end
