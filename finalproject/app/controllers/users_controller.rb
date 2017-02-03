0008ea3ac6ab58c82b5eb91828ae4dfde3007d40
shayboomin <karanshaylyn@gmail.com>
2017-02-03 09:36:17 -0800

    User

--- finalproject/app/controllers/users_controller.rb
+++ finalproject/app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
 	def new
     @user = User.new
-  end
+  	end
 
-  def create
-    @user = User.new(params[:user])
-      if @user.save
-            redirect_to :action => 'profile'
-      else
-            redirect_to :action => 'home'
-      end
+	def create
+	    @user = User.new(user_params)
+	      if @user.save
+	            # Handle successful save
+	      else
+	            redirect_to :action => 'home'
+	end
+	
+	def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- finalproject/app/models/user.rb
+++ finalproject/app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-	validates :name, :email, :password, presence: true
+
+	validates :name, :password, presence: true
 	attr_accessor :name, :email, :password, :htoelconfirm, :flightconfirm, :address, :birthday
-=======
+
   # name should be 2 characters
   # email should be 4 characters or more
   # email should be formatted like an email
 
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  
+  VALID_EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+                    uniqueness: { case_sensitive: false }
   # unique@uniqueIsCool.com
   # validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   # validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
-
-
-  validates :name, presence: true, length: { minimum: 2 }
->>>>>>> 5679264f2c2b7d98beef3a31393d095940ec8990
+  
 end
 
