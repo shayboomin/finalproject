8bd3c53a0724f9fe2292beec4c6b635c4d120727
ShadowSpyder <989075@p12fwps.org>
2017-02-16 11:45:20 -0800

    Update users_controller.rb

--- finalproject/app/controllers/users_controller.rb
+++ finalproject/app/controllers/users_controller.rb
@@ -13,7 +13,23 @@ class UsersController < ApplicationController
 			puts 'saving user', @user.inspect
 			puts '*'*80
 			redirect_to '/login'			
-		else
+		else 
+		
+			<% if flash[:errors] %>
+			<% flash[:errors].each do |error| %>
+				<p><%= error %></p>
+			<% end %>
+		<% end %>
+		
+		<%= simple_form_for @user do |f| %>
+			<%= f.input :name %>
+			<%= f.input :alias %>
+			<%= f.input :email %>
+			<%= f.input :password %>
+			<%= f.input :password_confirmation %>
+			<%= f.submit 'Register', :class => 'btn btn-danger' %>
+		<% end %>
+
 			# handle errors (unique)
 			puts 'not saving user', @user.errors.full_messages			
 			puts '-'*80			
