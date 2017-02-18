65b16883e46ebf099db47b4d38c1ee1b3fdd623b
shayboomin <karanshaylyn@gmail.com>
2017-02-18 14:43:04 -0800

    Session & Password

--- finalproject/app/controllers/users_controller.rb
+++ finalproject/app/controllers/users_controller.rb
@@ -13,9 +13,23 @@ class UsersController < ApplicationController
 		    else
 		    	redirect_to :action => 'home'
 		    end
+		if user = User.authenticate(params[:email], params[:password])
+			session[:current_user_id] = current_user
+			redirect_to 'profile'
 	end
 	
+	def destroy
+		session[:current_user_id] = nil
+		flash[:notice] = "No input"
+		redirect_to 'home'
+
 	def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    private
+    def current_user
+    	@current_user = session[:current_user_id]
+    	User.find_by(id: session[:current_user_id])
     end
 end
--- finalproject/app/models/user.rb
+++ finalproject/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
 	validates :name, :password, presence: true
-	attr_accessor :name, :email, :password, :hotelconfirm, :flightconfirm, :address, :birthday, :password_confirmation
+	# attr_accessor :name, :email, :password, :hotelconfirm, :flightconfirm, :address, :birthday, :password_confirmation
 
   # name should be 2 characters
   # email should be 4 characters or more
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   # validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   #has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
 
   
 end
