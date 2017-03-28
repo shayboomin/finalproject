d6aa7409eaaba3753d57295f8725782e53703890
Kris Ekenes <krisekenes@gmail.com>
2017-03-28 15:35:44 -0700

    added expedia and merged new homepage

--- finalproject/app/controllers/boomintravel_controller.rb
+++ finalproject/app/controllers/boomintravel_controller.rb
@@ -1,4 +1,17 @@
 class BoomintravelController < ApplicationController
+
+	Expedia.cid = 501048
+	Expedia.api_key = '60nn3elg2bfbcs1lsrmqu1l1oo'
+	Expedia.shared_secret = '3aaamcg1f416l'
+	Expedia.locale = 'en_US'
+	Expedia.currency_code = 'USD'
+	Expedia.minor_rev = 30
+	Expedia.use_signature = true # must be false if using ip whitelisting instead of signature
+	# Optional configuration...
+	Expedia.timeout = 1 # read timeout in sec
+	Expedia.open_timeout = 1 # connection timeout in sec
+	# Instentiate api object
+
 	def index
 		render 'home'
 	end
@@ -8,6 +21,22 @@ class BoomintravelController < ApplicationController
 		render 'login' 
 	end
 	def hotel
+		api = Expedia::Api.new
+		# Method to search for a hotel. see http://developer.ean.com/docs/hotel-list/
+		response = api.get_list({
+				:city => 'Seattle', 
+				:stateProvinceCode => 'WA', 
+				:countryCode=>"US", 
+				:arrivalDate => "09/04/2017", 
+				:departureDate=>"09/05/2017", 
+				:room1=>"1,3", 
+				:room2=>"1,5"
+			})
+
+		puts '/' *80
+		@hotelList = response.body["HotelListResponse"]["HotelList"]["HotelSummary"]
+		puts '/' *80
+
 		render 'hotel'
 	end
 	def flight
